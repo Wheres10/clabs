5ed7d376e0ed371432b2b7ea8a34f72fe86934fa
wheres10 <https://github.com/Wheres10/clabs.git>
2019-11-11 22:12:29 -0800

    111

---
+++ lab2 ex2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+   int num=0 ;
+   printf("Enter an integer");
+   scant("%d",&num);
+   printf("%d has %d 1 in the Binary sequence\n",num,bitcount(num));
+   system("pause");
+   return 0; 
+}
+int bitcount(int num)
+{
+    int t=0;
+    int i=0;
+    for(i=0;i<32;i++)
+    {
+        if((num&1)==1)
+        t++;
+        num=(num>>1);
+    }
+    return t;
+}
+
